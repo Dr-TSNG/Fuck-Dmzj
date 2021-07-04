0f3f05cdcf1ec5a60cc4aacdd59764a240eaf256
KyuubiRan <KyuubiRan@qq.com>
2021-07-04 21:10:30 +0800

    Update HttpUtil & lib version

--- app/src/main/java/com/tsng/fuckdmzj/utils/HttpUtils.kt
+++ app/src/main/java/com/tsng/fuckdmzj/utils/HttpUtils.kt
@@ -4,6 +4,10 @@ import okhttp3.*
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
+private typealias OnResponse = (call: Call, response: Response) -> Unit
+private typealias OnFailure = (call: Call, e: IOException) -> Unit
+
+
 object HttpUtils {
     private val okHttpClient = OkHttpClient.Builder().run {
         callTimeout(60, TimeUnit.SECONDS)
@@ -13,6 +17,27 @@ object HttpUtils {
         build()
     }
 
+    class CallbackFactory : Callback {
+        private var onResponse: OnResponse? = null
+        private var onFailure: OnFailure? = null
+
+        override fun onResponse(call: Call, response: Response) {
+            onResponse?.invoke(call, response)
+        }
+
+        override fun onFailure(call: Call, e: IOException) {
+            onFailure?.invoke(call, e)
+        }
+
+        fun response(onResponse: OnResponse) {
+            this.onResponse = onResponse
+        }
+
+        fun failure(onFailure: OnFailure) {
+            this.onFailure = onFailure
+        }
+    }
+
     //处理新请求
     fun execNewCall(req: Request): Response {
         return okHttpClient.newCall(req).execute()
@@ -20,5 +45,11 @@ object HttpUtils {
 
     fun execNewCall(req: Request, callback: Callback) {
         okHttpClient.newCall(req).enqueue(callback)
+    }
+
+    fun execNewCall(req: Request, logic: CallbackFactory.() -> Unit) {
+        val factory = CallbackFactory()
+        factory.logic()
+        okHttpClient.newCall(req).enqueue(factory)
     }
 }
